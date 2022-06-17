8a284553547f69c07a62e809a64d44e16966af6c
Pawan Kalyan <pawankalyan1614@gmail.com>
2022-06-17 14:48:53 +0530

    added command

--- test-bot-1/index.js
+++ test-bot-1/index.js
@@ -1,10 +1,23 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-const token = 'OTg3MDY3NDU3NDM5MTcwNjMw.GNrgof.cxctzxcg94pFwNx7WvIe_uGcwYytQu2rxK2WdM';
-const PREFIX = '';
+//Bot Token : OTg3MDY3NDU3NDM5MTcwNjMw.GNrgof.cxctzxcg94pFwNx7WvIe_uGcwYytQu2rxK2WdM
 
-bot.on('ready', () =>{
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const token = 'OTg3MDY3NDU3NDM5MTcwNjMw.GNrgof.cxctzxcg94pFwNx7WvIe_uGcwYytQu2rxK2WdM';
+
+bot.login(token);
+
+bot.on('ready', () => {
     console.log('This bot is online!');
 });
 
-bot.login(token);
+bot.on('message', message => {
+    if(message.author.bot) return;
+    if(message.channel.type !== 'text') return;
+    let PREFIX = '!';
+    if(!message.content.startsWith(PREFIX)) return;
+    let messageArray = message.content.split(' ');
+    let cmd = messageArray[0].slice(PREFIX.length);
+    if(cmd == 'hello') {
+        message.channel.send('Hello');
+    }
+});
