db388cf70adca20721a7db9391deedeff906806a
Pawan Kalyan <pawankalyan1614@gmail.com>
2022-06-17 17:51:04 +0530

    added comments

--- test-bot-1/index.js
+++ test-bot-1/index.js
@@ -1,23 +1,32 @@
 //Bot Token : OTg3MDY3NDU3NDM5MTcwNjMw.GNrgof.cxctzxcg94pFwNx7WvIe_uGcwYytQu2rxK2WdM
 
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-const token = 'OTg3MDY3NDU3NDM5MTcwNjMw.GNrgof.cxctzxcg94pFwNx7WvIe_uGcwYytQu2rxK2WdM';
+const Discord = require('discord.js'); //Discord.js
+const bot = new Discord.Client(); //Discord.js
+const fs = require('fs'); //File System
+const token = 'OTg3MDY3NDU3NDM5MTcwNjMw.GNrgof.cxctzxcg94pFwNx7WvIe_uGcwYytQu2rxK2WdM';   //Bot Token
+bot.commands = new Discord.Collection(); //Discord.js
 
-bot.login(token);
+bot.on('ready', () =>{ //Bot is ready
+    console.log('This bot is online!'); //Bot is online message
 
-bot.on('ready', () => {
-    console.log('This bot is online!');
+    fs.readdir('./commands', (err, files) => { //Reads the commands folder
+        if(err) return console.log(err); //if error in opening file
+        //hello.js => hello
+        let jsfile = files.filter(f => f.split('.').pop() == 'js'); //filter files to only get js files
+    });
 });
 
-bot.on('message', message => {
-    if(message.author.bot) return;
-    if(message.channel.type !== 'text') return;
-    let PREFIX = '!';
-    if(!message.content.startsWith(PREFIX)) return;
-    let messageArray = message.content.split(' ');
-    let cmd = messageArray[0].slice(PREFIX.length);
-    if(cmd == 'hello') {
-        message.channel.send('Hello');
+bot.on('message', (message) => { //Bot is messaged
+    let PREFIX = '!';  //Prefix
+    let messageArray = message.content.split(" "); //spliting the message into array
+    if(message.author.bot) { return; }; //if author of the message is bot
+    if(!message.content.startsWith(PREFIX)) { return; }; //if message does not starts with '!'
+    if(message.channel.type !== 'text') { return; }; //if message is not in text channel
+    let cmd = messageArray[0].slice(PREFIX.length); //getting the command by removing '!' from the first element of the array
+    if(cmd == 'hello' || cmd == 'hey'){  //if command is 'hello' or 'hey'
+        let member = message.mentions.members.first(); //getting the mentioned member if any in the message
+        if(!member) {message.channel.send('Hello!');} else {message.channel.send('hello '+member.user.tag)}; //if no member is mentioned, send 'hello' else send 'hello '+member.user.tag (user and tage)
     }
 });
+
+bot.login(token); //login the bot with the token
