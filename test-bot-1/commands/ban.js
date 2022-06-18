c43114d0e10f00fff759fe8c64a72c8144a7e526
Pawan Kalyan <pawankalyan1614@gmail.com>
2022-06-18 15:32:14 +0530

    added mute command

--- test-bot-1/commands/ban.js
+++ test-bot-1/commands/ban.js
@@ -4,9 +4,9 @@ exports.run = async(bot, message, args) => {
     if(!member) { return message.channel.send('Invalid Member Given');}
     if(member.role.highest.position > message.member.role.highest.position) { return message.channel.send('You cannot ban this member.')};
     let days = args[1];
-    if(!(Number.isInteger(days) && int(days)  > 0 )) { return message.channel.send('Please provide a valid number of days.');}
+    if(!Number.isInteger(days) || days < 1) { return message.channel.send('Please provide a valid number of days. \n `!ban [@mention] [days] [reason]`');}
     let reason = args.slice(2).join(' ');
-    if(!reason) { return message.channel.send('Please provide a reason');}
+    if(!reason) { return message.channel.send('Please provide a reason \n `!ban [@mention] [days] [reason]`');}
     member.ban({days : days, reason : reason});
     message.channel.send(`Kicked **${member.user.tag}** for **${reason}**`);
 }
--- test-bot-1/commands/kick.js
+++ test-bot-1/commands/kick.js
@@ -4,7 +4,7 @@ exports.run = async(bot, message, args) => {
     if(!member) { return message.channel.send('Invalid Member Given');}
     if(member.role.highest.position > message.member.role.highest.position) { return message.channel.send('You cannot ban this member.')};
     let reason = args.slice(1).join(' ');
-    if(!reason) { return message.channel.send('Please provide a reason');}
+    if(!reason) { return message.channel.send('Please provide a reason \b `!kick [@member] [reason]`');}
     member.kick(reason);
     message.channel.send(`Kicked **${member.user.tag}** for **${reason}**`);
 }
