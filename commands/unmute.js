d9adf5ed5ab95034df5399edc871f91765af9ac1
Pawan Kalyan <pawankalyan1614@gmail.com>
2022-06-26 00:23:38 +0530

    added unmute command

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,7 +1,8 @@
 exports.run = async(bot, message, args) => {
-    if(!message.member.hasPermission("MANAGE_MESSAGES") || !message.member.hasPermission("OWNER")) { return message.channel.send("You dont have permission to excute this command!") }; //This is the code that checks if the user has the permission to use the command.
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) { return message.channel.send("You dont have permission to excute this command!") }; //This is the code that checks if the user has the permission to use the command.
     let member = message.mentions.members.first();
     let MuteRole = message.guild.roles.cache.find(r => r.name === "Mute");
+    if(!MuteRole){ return message.channel.send("`Mute` Role not found"); }
     if(!member){ return message.channel.send("Please meantion a valid member"); }
     if(member.roles.cache.has(MuteRole.id)){
         member.roles.remove(MuteRole.id);
